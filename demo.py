bb43ece1b70968d17bd88ef105d4ed6b8fd2815a
Mac-Lee0502 <qq1157725063@outlook.com>
2020-01-14 11:07:11 +0800

    selection sort
    Resolve #2
    
    Signed-off-by: Mac-Lee0502 <qq1157725063@outlook.com>

--- demo.py
+++ demo.py
@@ -1,2 +1,15 @@
-def foo():
-    return False
+def select_sort(arr):
+    i = 0
+    while i < len(arr) - 1:
+        j = i + 1
+        max = i
+
+        while j < len(arr):
+            if arr[j] > arr[max]:
+                max = j
+            j += 1
+
+        if max != i:
+            arr[max], arr[i] = arr[i], arr[max]
+
+        i += 1
--- test_demo.py
+++ test_demo.py
@@ -1,5 +1,13 @@
 from demo import *
+import random
 
-def test_foo():
-    res = foo()
-    assert(res == True)
+def test_select_sort():
+    for x in range(10000):
+        arr = [random.randint(10, 99) for _ in range(10)]
+
+        select_sort(arr)
+
+        i = 0
+        while i < len(arr) - 1:
+            assert(arr[i] >= arr[i + 1])
+            i += 1
